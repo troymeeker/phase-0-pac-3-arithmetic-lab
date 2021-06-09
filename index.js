bfa4ffa32ebb65b171406aacdd17f47598b78b84
troymeeker <troymeeker1@gmail.com>
2021-06-08 17:16:51 -0700

    Completed assignment

---
+++ index.js
@@ -0,0 +1,37 @@
+function add(a,b){
+ return a + b
+}
+add(1,6);
+
+function subtract(a,b){
+    return a - b
+}
+subtract(5,2);
+
+function multiply(a,b){
+ return a * b
+} multiply(10,2);
+
+function divide(a,b){
+    return a / b
+
+} divide(10,5);
+
+function increment(n){
+ return n += 1;
+}
+increment(10);
+
+function decrement(n){
+  return n -= 1;
+}
+decrement(10);
+
+function makeInt(n){
+    return parseInt(n, 10)
+}
+
+
+function preserveDecimal(n){
+    return parseFloat(n)
+}
